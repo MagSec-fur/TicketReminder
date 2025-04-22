f7312d522cefe431480b617a548e2cf8697d825b
magonyfur <magony.fur@gmail.com>
2025-04-22 15:48:27 +0200

    Update config.form.php

--- front/config.form.php
+++ front/config.form.php
@@ -1,7 +1,7 @@
 <?php
 include ('../../../inc/includes.php');
 Session::checkRight("config", UPDATE);
-$csrf_token = Session::getNewCSRFToken();
+
 if (isset($_POST['update'])) {
     // CSRF check
     Session::checkCSRF([
@@ -18,7 +18,6 @@ echo "<table class='tab_cadre_fixe'>";
 echo "<tr><th colspan='2'>Settings</th></tr>";
 echo "<tr class='tab_bg_1'><td>Days</td><td><input type='number' name='days' min='0' max='365' value='3'></td></tr>";
 echo "<tr class='tab_bg_1'><td>Hours</td><td><input type='number' name='hours' min='0' max='23' value='0'></td></tr>";
-echo Html::hidden('_glpi_csrf_token', ['value' => $csrf_token]);
 echo "<tr class='tab_bg_2'><td colspan='2' class='center'><input type='submit' name='update' value='Save' class='submit'></td></tr>";
 echo "</table>";
 echo "</form>";
