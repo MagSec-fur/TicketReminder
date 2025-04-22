6fe2ec689ad2b471159d9d852c246b9028fb8c7c
magonyfur <magony.fur@gmail.com>
2025-04-22 15:36:14 +0200

    V1.0.3.1

--- front/config.form.php
+++ front/config.form.php
@@ -1,24 +1,27 @@
 <?php
 include ('../../../inc/includes.php');
-Session::checkRight("config", READ);
-
-$config = PluginTicketreminderConfig::getConfig(0);
-
+Session::checkRight("config", UPDATE);
+$csrf_token = Session::getNewCSRFToken();
 if (isset($_POST['update'])) {
-    $days = max(0, (int)$_POST['days']);
-    $hours = max(0, min(23, (int)$_POST['hours']));
-    PluginTicketreminderConfig::setConfig(0, $days, $hours);
-    Html::displayMessageAfterRedirect(__('Configuration saved!', 'ticketreminder'), true, $_SERVER['PHP_SELF']);
+    // CSRF check
+    Session::checkCSRF([
+        '_glpi_csrf_token' => $_POST['_glpi_csrf_token']
+    ]);
+    // Save config here...
+    Html::redirect($_SERVER['PHP_SELF']);
 }
 
 Html::header(__('Ticket Reminder Configuration', 'ticketreminder'), $_SERVER['PHP_SELF'], 'config', 'plugin');
+
 echo "<form method='post'>";
 echo "<table class='tab_cadre_fixe'>";
-echo "<tr><th colspan='2'>".__('Reminder Threshold Configuration', 'ticketreminder')."</th></tr>";
-echo "<tr class='tab_bg_1'><td>".__('Days', 'ticketreminder')."</td><td><input type='number' name='days' min='0' max='365' value='".(int)$config['days']."'></td></tr>";
-echo "<tr class='tab_bg_1'><td>".__('Hours', 'ticketreminder')."</td><td><input type='number' name='hours' min='0' max='23' value='".(int)$config['hours']."'></td></tr>";
-echo "<tr class='tab_bg_2'><td colspan='2' class='center'><input type='submit' name='update' value='"._sx('button', 'Save')."' class='submit'></td></tr>";
+echo "<tr><th colspan='2'>Settings</th></tr>";
+echo "<tr class='tab_bg_1'><td>Days</td><td><input type='number' name='days' min='0' max='365' value='3'></td></tr>";
+echo "<tr class='tab_bg_1'><td>Hours</td><td><input type='number' name='hours' min='0' max='23' value='0'></td></tr>";
+echo Html::hidden('_glpi_csrf_token', ['value' => $csrf_token]);
+echo "<tr class='tab_bg_2'><td colspan='2' class='center'><input type='submit' name='update' value='Save' class='submit'></td></tr>";
 echo "</table>";
 echo "</form>";
+Html::closeForm();
 Html::footer();
 ?>
